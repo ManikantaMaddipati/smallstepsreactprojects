42a42222192868cc84a7bc51ffeb1964bc74cffb
manikanta <manikanta3517@gmail.com>
2020-11-28 04:37:58 -0500

    added usestate and useref hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
+import React,{useState,useRef} from 'react';
+
+
+function helpPadTime(time) {
+    return time.toString().padStart(2, '0');
+
+}
 function App() {
-  return (
+
+    const [title,settitle] = useState("Start The Timer!");
+
+
+    const [timeleft,setTimeleft] = useState(25 * 60);
+
+    const intervalRef =useRef(null);
+
+    const minutes = helpPadTime(Math.floor(timeleft/60));
+    const seconds = helpPadTime(timeleft - minutes * 60);
+
+    function startTimer() {
+        settitle('Timer Started');
+        intervalRef.current =  setInterval(
+           () => setTimeleft( (timeleft) => {
+                   if (timeleft >= 1) return timeleft - 1;
+
+                   return 0;
+               }
+           ),1000
+       )
+    }
+
+    function stopTimer() {
+        clearInterval(intervalRef.current);
+        settitle('Timer Stopped');
+    }
+
+    return (
     <div className="App">
-      <h1>Hello world!</h1>
+        <nav>
+            <p className="app-description">App <span className="app-number">#1</span> Simple Timer.</p>
+        </nav>
+
+        <main className="main">
+            <h1 className="main-h1"> <span>{title}</span> </h1>
+
+
+          <div className="timer">
+            <div className="timerSection">
+                <span>{minutes}</span>
+                <span>:</span>
+                <span>{seconds}</span>
+            </div>
+
+            <div className="timerButtons">
+               <button className="startButton" onClick={startTimer}>START</button>
+                <button className="resetStop" onClick={stopTimer}>STOP</button>
+               <button className="resetButton">RESET</button>
+            </div>
+          </div>
+
+        </main>
     </div>
   );
 }
